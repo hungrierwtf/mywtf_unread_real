7c87cca7bb1569e2279b738c92d0e32e0a40a2e3
hungrier <hungrierwtf@gmail.com>
2020-04-10 14:38:10 -0400

    committed to github

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mywtf_unread_real
 Marks topics that are not read all the way so they show up on the topic list
+
+# How to use
+Add this to your custom CSS:
+```
+.category>ul>li.half-read h2 a {
+    color: #8ae;
+}
+```
+
+Topics that aren't read all the way will show up with a different colour on the topic list.
---
+++ wtfunread.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         mywtf_unread_real
+// @namespace    WTDWTF
+// @version      0.6
+// @description  mark topics unread that are read but not all the way
+// @author       hungrier
+// @match        https://what.thedailywtf.com/*
+// @match        http://what.thedailywtf.com/*
+// @grant        none
+// @downloadURL  https://github.com/hungrierwtf/mywtf_unread_real/raw/master/wtfunread.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function markUnreadTopics() {
+        //console.log('marking unread');
+        var topicsNodeList = document.querySelectorAll('ul.topic-list li:not(.unread)');
+        var topics = Array.from(topicsNodeList);
+        var incompleteTopics = topics.filter((t)=> {
+            var titleUrl = t.querySelector('a[itemprop=url]').href;
+            var previewUrl = t.querySelector('a.permalink').href;
+            return !t.classList.contains('half-read') && titleUrl !== previewUrl;
+        });
+
+        incompleteTopics.map((t)=>{
+            t.classList.add('half-read');
+        });
+    }
+
+    $(window).on('action:topics.loaded', markUnreadTopics);
+})();
