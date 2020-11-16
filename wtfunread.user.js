4de586e143b554d0151dee9b628b2250f9400a7c
hungrier <hungrierwtf@gmail.com>
2020-11-16 12:54:45 -0500

    added regex to find post number

--- wtfunread.user.js
+++ wtfunread.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mywtf_unread_real
 // @namespace    WTDWTF
-// @version      0.6.1
+// @version      0.6.2
 // @description  mark topics unread that are read but not all the way
 // @author       hungrier
 // @match        https://what.thedailywtf.com/*
@@ -11,8 +11,38 @@
 // @homepageURL  https://github.com/hungrierwtf/mywtf_unread_real/
 // ==/UserScript==
 
+
+// how to do it for real
+// get all untagged* topics
+// for each one
+//   determine last read and latest post number using the preview url/title url
+//   tag it:
+//     new topic / new posts / half-read / fully read
+//     how many more posts
+//     and the css of course
+//   for new topics, stick a "NEW!" starburst or something there
+//   for not fully read ones, put the number of new posts
+// it'll be great
+
+
 (function() {
     'use strict';
+
+    // https://what.thedailywtf.com/topic/27647/re-welcome-to-the-lounge/9
+    // https://what.thedailywtf.com/topic/21563/the-unofficial-funny-comic-strips-thread/726
+    // https://what.thedailywtf.com/topic/27631/anyone-with-3rd-gen-ryzen-to-borrow/58
+    // https://what.thedailywtf.com/topic/27652/what-is-evidence
+    //                             /topic/\d+/[^/]+(/(\d+))?
+    const regex = /\/topic\/\d+\/[^\/]+(\/(\d+))?/m;
+
+    function getTopicPostNumber(url) {
+        let m = regex.exec(url);
+        if (m && m.length > 2 && m[2]) {
+            return m[2];
+        } else {
+            return '1';
+        }
+    }
 
     function markUnreadTopics() {
         //console.log('marking unread');
@@ -21,7 +51,9 @@
         var incompleteTopics = topics.filter((t)=> {
             var titleUrl = t.querySelector('a[itemprop=url]').href;
             var previewUrl = t.querySelector('a.permalink').href;
-            return !t.classList.contains('half-read') && titleUrl !== previewUrl;
+            let titleNum = getTopicPostNumber(titleUrl);
+            let prevNum = getTopicPostNumber(previewUrl);
+            return !t.classList.contains('half-read') && prevNum !== titleNum;
         });
 
         incompleteTopics.map((t)=>{
